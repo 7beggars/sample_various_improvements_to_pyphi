f5e6911b50f5081013b8af74080943ba17f23e2e
2kmountains <76716532+yaacovNaNachRabbeinu@users.noreply.github.com>
2021-12-11 18:51:04 -0300

    Create mechanisms.py

---
+++ mechanisms.py
@@ -0,0 +1,80 @@
+# Lambdas avoided for easier debugging
+
+from toolz import curry
+import numpy as np
+
+def AND(inputs):
+    return sum(inputs) == len(inputs)
+
+
+def NAND(inputs):
+    return not AND(inputs)
+
+
+def OR(inputs):
+    return sum(inputs) >= 1
+
+
+def COPY(inputs):
+    assert len(inputs) <= 1, "COPY gates cannot have multiple inputs"
+    return OR(inputs)
+
+
+def NOR(inputs):
+    return not OR(inputs)
+
+
+def NOT(inputs):
+    assert len(inputs) <= 1, "NOT gates cannot have multiple inputs"
+    return NOR(inputs)
+
+
+def XOR(inputs):
+    return sum(inputs) % 2 == 1
+
+def XNOR(inputs):
+    return not XOR(inputs)
+
+def MAJORITY(inputs):
+    return sum(inputs) > len(inputs)/2
+
+
+MAJ = MAJORITY
+
+
+def MINORITY(inputs):
+    return sum(inputs) <= len(inputs)/2
+
+
+MIN = MINORITY
+
+
+def PARITY(inputs):
+    return sum(inputs) % 2 == 0
+
+
+def PAR(inputs):
+    return PARITY(inputs)
+
+@curry
+def NAKA_RUSHTON(threshold, exponent, weights, inputs):
+    input_strength = np.sum(np.array(weights) * np.array(inputs))**exponent
+    return input_strength / (threshold**exponent + input_strength)
+
+MAP = {
+    'AND': AND,
+    'NAND': NAND,
+    'OR': OR,
+    'NOR': NOR,
+    'COPY': COPY,
+    'NOT': NOT,
+    'XOR': XOR,
+    'XNOR': XNOR,
+    'MAJORITY': MAJORITY,
+    'MAJ': MAJ,
+    'MINORITY': MINORITY,
+    'MIN': MIN,
+    'PARITY': PARITY,
+    'PAR': PAR,
+    'NAKA_RUSHTON': NAKA_RUSHTON,    
+}
